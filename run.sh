47d234bf808809d01784a6d8526a1b1ed57143b5
Shuwei Zeng <uwszeng@gmail.com>
2018-10-29 13:03:04 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-#python ./src/h1b_topmetrics.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python ./src/h1b_topmetrics.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
